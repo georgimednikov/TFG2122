6b0abb6d12277e0a30ca25b565218afda92e32d8
Daniel González Cerdeiras <daniel20@ucm.es>
2022-02-19 19:56:23 +0100

    Distribución de criaturas por el terreno.

---
+++ PGEBIELBNE/Assets/EvolutionSimulation/GenerateCreatures.cs
@@ -0,0 +1,56 @@
+using System;
+using UnityEngine;
+
+namespace EvolutionSimulation.Unity
+{
+    public class GenerateCreatures : MonoBehaviour
+    {
+        [Serializable]
+        public struct CreatureSpawn
+        {
+            public TextAsset species;
+            public int amount;
+            public float positionX;
+            public float positionZ;
+            public float positionRadius;
+        }
+
+        public GameObject creature;
+        
+        public CreatureSpawn[] creatureDistribution;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Terrain terrain = GetComponent<Terrain>();
+            System.Random rnd = new System.Random();
+
+            foreach (CreatureSpawn spawn in creatureDistribution)
+            {
+                for (int i = 0; i < spawn.amount; ++i)
+                {
+                    if (spawn.positionRadius > terrain.terrainData.size.x / 2 || spawn.positionRadius > terrain.terrainData.size.z / 2)
+                        throw new Exception("The radius for the spawn of creatures cannot surpass the size of the world in neither the axis X or Z");
+
+                    if (spawn.positionX < transform.position.x + spawn.positionRadius || spawn.positionX > transform.position.x + terrain.terrainData.size.x - spawn.positionRadius ||
+                        spawn.positionZ < transform.position.z + spawn.positionRadius || spawn.positionZ > transform.position.z + terrain.terrainData.size.z - spawn.positionRadius)
+                        throw new Exception("The position given to spawn any species must be within the logical confines of the world given the radius of spawn");
+
+                    // Calculates a random point within a circle to get the offset of the position.
+                    float xPos = (float)((spawn.positionRadius * Math.Sqrt(rnd.NextDouble())) * Math.Cos(rnd.NextDouble() * 2 * Math.PI));
+                    float zPos = (float)((spawn.positionRadius * Math.Sqrt(rnd.NextDouble())) * Math.Sin(rnd.NextDouble() * 2 * Math.PI));
+
+                    // Adds the center of the circle to offset the position to the final value.
+                    xPos += spawn.positionX;
+                    zPos += spawn.positionZ;
+
+                    RaycastHit hit;
+                    // Position in Y axis + 10 (no specific reason for that number) => No point in the world will be higher.  
+                    Physics.Raycast(new Vector3(xPos, terrain.terrainData.size.y + 10, zPos), Vector3.down, out hit, Mathf.Infinity, LayerMask.GetMask("World"));
+                    InterpretJSON json = Instantiate(creature, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.identity).GetComponent<InterpretJSON>();
+                    json.CreateCreature(spawn.species);
+                }
+            }
+        }
+    }
+}
--- PGEBIELBNE/Assets/EvolutionSimulation/InterpretJSON.cs
+++ PGEBIELBNE/Assets/EvolutionSimulation/InterpretJSON.cs
@@ -1,20 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace EvolutionSimulation.Unity
 {
     public class InterpretJSON : MonoBehaviour
     {
-        //[HideInInspector]
-        public TextAsset json;
         public GameObject body;
         public GameObject leg;
         public GameObject[] mouths;
         public GameObject armour;
 
         // Start is called before the first frame update
-        void Awake()
+        public void CreateCreature(TextAsset json)
         {
             SpeciesExport creature = JsonUtility.FromJson<SpeciesExport>(json.text);
 
@@ -31,29 +27,29 @@ namespace EvolutionSimulation.Unity
         {
             GameObject go = Instantiate(mouths[(int)creature.stats.Diet], transform);
             go.transform.localScale = Vector3.one * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f;
-            go.transform.position = Vector3.up * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f * 1.5f;
+            go.transform.localPosition = Vector3.up * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f * 1.5f;
         }
 
         private void InstantiateArmour(SpeciesExport creature)
         {
             GameObject go = Instantiate(armour, transform);
             go.transform.localScale = Vector3.one * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f;
-            go.transform.position = Vector3.up * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f;
+            go.transform.localPosition = Vector3.up * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f;
         }
 
         private void InstantiateBody(SpeciesExport creature)
         {
             GameObject go = Instantiate(body, transform);
             go.transform.localScale = Vector3.one * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f;
-            go.transform.position = Vector3.up * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f;
+            go.transform.localPosition = Vector3.up * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f;
         }
 
         private void InstantiateLegs(SpeciesExport creature)
         {
             GameObject go;
             GameObject legParent = new GameObject("Leg Cluster");
-            legParent.transform.localPosition = Vector3.up * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f;
             legParent.transform.parent = transform;
+            legParent.transform.localPosition = Vector3.up * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f;
             legParent.transform.localScale = Vector3.one * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f;
 
             float deltaAngle = 2 * Mathf.PI / creature.stats.Members;
