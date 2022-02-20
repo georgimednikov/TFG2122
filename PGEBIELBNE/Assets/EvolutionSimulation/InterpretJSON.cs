9807799d8ddded551dd113096495589b660b4530
Eloy Moreno <eloymore@ucm.es>
2022-02-20 11:37:53 +0100

    Puestas mas piezas a la criatura en Unity

--- PGEBIELBNE/Assets/EvolutionSimulation/InterpretJSON.cs
+++ PGEBIELBNE/Assets/EvolutionSimulation/InterpretJSON.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace EvolutionSimulation.Unity
@@ -7,7 +8,11 @@ namespace EvolutionSimulation.Unity
         public GameObject body;
         public GameObject leg;
         public GameObject[] mouths;
-        public GameObject armour;
+        public GameObject[] armours;
+        public GameObject spikes;
+        public GameObject hair;
+        public GameObject beard;
+        public GameObject statusBar;
 
         // Start is called before the first frame update
         public void CreateCreature(TextAsset json)
@@ -21,6 +26,14 @@ namespace EvolutionSimulation.Unity
             InstantiateMouth(creature);
 
             InstantiateArmour(creature);
+
+            InstantiateSpikes(creature);
+
+            // InstantiateHair(creature);
+
+            InstantiateBeard(creature);
+
+            InstantiateStatusBar(creature);
         }
 
         private void InstantiateMouth(SpeciesExport creature)
@@ -32,7 +45,12 @@ namespace EvolutionSimulation.Unity
 
         private void InstantiateArmour(SpeciesExport creature)
         {
-            GameObject go = Instantiate(armour, transform);
+            GameObject go;
+            if (creature.stats.Armor >= 15)
+                go = Instantiate(armours[1], transform);
+            else if (creature.stats.Armor >= 8)
+                go = Instantiate(armours[0], transform);
+            else return; // If Armor is low, don't put on armor
             go.transform.localScale = Vector3.one * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f;
             go.transform.localPosition = Vector3.up * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f;
         }
@@ -62,6 +80,37 @@ namespace EvolutionSimulation.Unity
                 go.transform.rotation = Quaternion.Euler(0, -i * deltaAngle / Mathf.PI * 180, -45);
             }
             if (creature.stats.Members % 2 == 1) legParent.transform.Rotate(new Vector3(0, 90, 0));
+        }
+
+        private void InstantiateSpikes(SpeciesExport creature)
+        {
+            if (creature.stats.Counter == -1) return; // TODO: how to check if the creture has this ability?
+            GameObject go = Instantiate(spikes, transform);
+            go.transform.localScale = Vector3.one * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f;
+            go.transform.localPosition = Vector3.up * 1.5f * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f;
+        }
+
+        private void InstantiateHair(SpeciesExport creature)
+        {
+            // TODO: how to know if it has hair
+            //if (creature.stats. == -1) return;
+            GameObject go = Instantiate(hair, transform);
+            go.transform.localScale = Vector3.one * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f;
+        }
+
+        private void InstantiateBeard(SpeciesExport creature)
+        {
+            if (creature.stats.LifeSpan <= 30 * 50 * 24 * 365) return; // TODO: this time configurable with how much is a year
+            GameObject go = Instantiate(beard, transform);
+            go.transform.localScale = Vector3.one * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f;
+        }
+
+        private void InstantiateStatusBar(SpeciesExport creature)
+        {
+            StatusBar go = Instantiate(statusBar, transform).GetComponent<StatusBar>();
+            go.SetName(creature.name);
+            go.transform.localScale = Vector3.one * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f;
+            go.transform.localPosition = Vector3.up * 3 * creature.stats.ModifyStatByAge(creature.stats.Size) / 100f;
         }
     }
 }
