be185ec444e7e1d4b296efb2d5e59e6a22f7d983
Daniel González Cerdeiras <daniel20@ucm.es>
2022-03-02 10:22:41 +0100

    GoToDrink hecho en teoría.

--- Evolution/EvolutionSimulation/src/FSM/Creature/States/Safe/GoToDrink.cs
+++ Evolution/EvolutionSimulation/src/FSM/Creature/States/Safe/GoToDrink.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace EvolutionSimulation.FSM.Creature.States
 {
@@ -8,28 +9,76 @@ namespace EvolutionSimulation.FSM.Creature.States
 
         public override int GetCost()
         {
-            return(int)(1000 * ((200f - creature.stats.GroundSpeed) / 100f));
+            return creature.GetNextCostOnPath();
+        }
+
+        public override void OnEntry()
+        {
+            Tuple<int, int> posToDrink = creature.GetClosestWater();
+            //Angle between the creatures position and the position of the water from 0 to 180 positive or negative.
+            double degrees = Math.Atan2(posToDrink.Item2 - creature.y, posToDrink.Item1 - creature.x) * (180 / Math.PI);
+
+            //Now the sector has to be accounted for.
+
+            //If the resulting angle is negative it is converted to a degree from 0 to 360
+            if (degrees < 0) degrees += 360;
+
+            //There are 8 sectors representing the 8 tiles that surround another, each one with an angle of 360/8 = 45 degrees.
+            //Half a quadrant of offset to start counting the angle each sector represents from 0.
+            degrees += 45.0 / 2;
+
+            int sector = (int)(degrees / 45);
+            Tuple<int, int> finalPosition = SectorToPosition(sector);
+            int cont = 0;
+            while (creature.world.map[finalPosition.Item1, finalPosition.Item2].isWater)
+            {
+                //The increment has to have the same sign as cont to add their values without possible substractions,
+                //but cont's sign has to be mantained to alternate between going "left" or "right" realtive to the current sector.
+                int inc = 1; if (cont < 0) inc *= -1;
+                cont = (cont + inc) * -1;
+                sector = (sector + cont) % 8;
+                finalPosition = SectorToPosition(sector);
+            }
+
+            creature.SetPath(finalPosition.Item1, finalPosition.Item2);
         }
 
         public override void Action()
         {
-            //int nX = creature.objective.x - creature.x,
-            //    nY = creature.objective.y - creature.y;
-            //nX = nX > 0 ? 1 : (nX < 0 ? -1 : 0);
-            //nY = nY > 0 ? 1 : (nY < 0 ? -1 : 0);
-            //// TODO: A-estrella ?
+            Console.WriteLine("Go to water action");
 
-            //if (creature.world.canMove(nX, nY)) // TODO: ahora mismo si encuentra un obstaculo no hace nada
-            //{
-            //    creature.Place(nX, nY);
-            //    return (int)(1000 * ((200f - creature.stats.GroundSpeed) / 100f)); // Cost of the action performed
-            //}
-            Console.WriteLine("GoToDrink action");
+            Vector3 nextPos = creature.GetNextPosOnPath();
+            creature.Place((int)nextPos.X, (int)nextPos.Y, (Entities.Creature.HeightLayer)nextPos.Z);
         }
 
         public override string ToString()
         {
             return "GoToDrinkState";
+        }
+
+        private Tuple<int, int> SectorToPosition(int sector)
+        {
+            switch (sector)
+            {
+                case 0:
+                    return new Tuple<int, int>(0, 1);
+                case 1:
+                    return new Tuple<int, int>(1, 1);
+                case 2:
+                    return new Tuple<int, int>(0, 1);
+                case 3:
+                    return new Tuple<int, int>(-1, 1);
+                case 4:
+                    return new Tuple<int, int>(-1, 0);
+                case 5:
+                    return new Tuple<int, int>(-1, -1);
+                case 6:
+                    return new Tuple<int, int>(0, -1);
+                case 7:
+                    return new Tuple<int, int>(1, -1);
+                default:
+                    return new Tuple<int, int>(-1, -1);
+            }
         }
     }
 }
--- Evolution/VisualizadorConsola/Program.cs
+++ Evolution/VisualizadorConsola/Program.cs
@@ -1,4 +1,5 @@
 ﻿using EvolutionSimulation;
+using System;
 
 namespace VisualizadorConsola
 {
@@ -8,14 +9,22 @@ namespace VisualizadorConsola
         {
 #if DEBUG
             UserInfo.SetDebugInfo();
+            Console.WriteLine(Math.Atan2(0, 1) * (180 / Math.PI));
+            Console.WriteLine(Math.Atan2(1, 1) * (180 / Math.PI));
+            Console.WriteLine(Math.Atan2(1, 0) * (180 / Math.PI));
+            Console.WriteLine(Math.Atan2(1, -1) * (180 / Math.PI));
+            Console.WriteLine(Math.Atan2(0, -1) * (180 / Math.PI));
+            Console.WriteLine(Math.Atan2(-1, -1) * (180 / Math.PI));
+            Console.WriteLine(Math.Atan2(-1, 0) * (180 / Math.PI));
+            Console.WriteLine(Math.Atan2(-1, 1) * (180 / Math.PI));
 #else
             if (!UserInfo.AskInfoUsingConsole())
                 return;
 #endif
-            EvolutionSimulation.Genetics.CreatureChromosome.SetChromosome();
-            ISimulation s = new ConsoleSimulation();
-            s.Init();
-            s.Run();
+            //EvolutionSimulation.Genetics.CreatureChromosome.SetChromosome();
+            //ISimulation s = new ConsoleSimulation();
+            //s.Init();
+            //s.Run();
         }
     }
 }
