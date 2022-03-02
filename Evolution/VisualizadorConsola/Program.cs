b38f87eb63b86d9eb41cfbdefdf1330dac08c03c
Daniel González Cerdeiras <daniel20@ucm.es>
2022-03-02 10:23:57 +0100

    Whoops

--- Evolution/VisualizadorConsola/Program.cs
+++ Evolution/VisualizadorConsola/Program.cs
@@ -9,22 +9,14 @@ namespace VisualizadorConsola
         {
 #if DEBUG
             UserInfo.SetDebugInfo();
-            Console.WriteLine(Math.Atan2(0, 1) * (180 / Math.PI));
-            Console.WriteLine(Math.Atan2(1, 1) * (180 / Math.PI));
-            Console.WriteLine(Math.Atan2(1, 0) * (180 / Math.PI));
-            Console.WriteLine(Math.Atan2(1, -1) * (180 / Math.PI));
-            Console.WriteLine(Math.Atan2(0, -1) * (180 / Math.PI));
-            Console.WriteLine(Math.Atan2(-1, -1) * (180 / Math.PI));
-            Console.WriteLine(Math.Atan2(-1, 0) * (180 / Math.PI));
-            Console.WriteLine(Math.Atan2(-1, 1) * (180 / Math.PI));
 #else
             if (!UserInfo.AskInfoUsingConsole())
                 return;
 #endif
-            //EvolutionSimulation.Genetics.CreatureChromosome.SetChromosome();
-            //ISimulation s = new ConsoleSimulation();
-            //s.Init();
-            //s.Run();
+            EvolutionSimulation.Genetics.CreatureChromosome.SetChromosome();
+            ISimulation s = new ConsoleSimulation();
+            s.Init();
+            s.Run();
         }
     }
 }
