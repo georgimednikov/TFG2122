34acae69035ee92099b2d5907c6d8cd2465b2238
georgimednikov <gmedniko@ucm.es>
2022-03-03 10:52:18 +0100

    Hecho que A* tenga en cuenta el peligro
    No lo he podido testear de momento.
    Además, he agregado lo de las excepciones custom como dijo Cleon

--- Evolution/EvolutionSimulation/src/Validator.cs
+++ Evolution/EvolutionSimulation/src/Validator.cs
@@ -12,6 +12,43 @@ namespace EvolutionSimulation
     public static class Validator
     {
         #region WorldGenConfig
+        public class NonZeroSizeMapException : Exception
+        {
+            public NonZeroSizeMapException() { }
+            public NonZeroSizeMapException(string message) : base(message) { }
+            public NonZeroSizeMapException(string message, Exception inner) : base(message, inner) { }
+            protected NonZeroSizeMapException(
+              System.Runtime.Serialization.SerializationInfo info,
+              System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+        }
+        public class NotSameSizeMapsException : Exception
+        {
+            public NotSameSizeMapsException() { }
+            public NotSameSizeMapsException(string message) : base(message) { }
+            public NotSameSizeMapsException(string message, Exception inner) : base(message, inner) { }
+            protected NotSameSizeMapsException(
+              System.Runtime.Serialization.SerializationInfo info,
+              System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+        }
+        public class FunctionNotWhollyDefinedException : Exception
+        {
+            public FunctionNotWhollyDefinedException() { }
+            public FunctionNotWhollyDefinedException(string message) : base(message) { }
+            public FunctionNotWhollyDefinedException(string message, Exception inner) : base(message, inner) { }
+            protected FunctionNotWhollyDefinedException(
+              System.Runtime.Serialization.SerializationInfo info,
+              System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+        }
+        public class FunctionIncorrectOutputRangeException : Exception
+        {
+            public FunctionIncorrectOutputRangeException() { }
+            public FunctionIncorrectOutputRangeException(string message) : base(message) { }
+            public FunctionIncorrectOutputRangeException(string message, Exception inner) : base(message, inner) { }
+            protected FunctionIncorrectOutputRangeException(
+              System.Runtime.Serialization.SerializationInfo info,
+              System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+        }
+        
         static double funcValidationAccuracy = 0.1;
         static public void Validate(WorldGenConfig configuration)
         {
@@ -27,7 +64,7 @@ namespace EvolutionSimulation
         static void NonZeroSize(WorldGenConfig config)
         {
             if (config.humidityMap != null || config.temperatureMap != null || config.heightMap != null) return;
-            if (config.mapSize <= 0) throw new ApplicationException("The provided size is equal or less than 0");
+            if (config.mapSize <= 0) throw new NonZeroSizeMapException("The provided size is equal or less than 0");
 
             return;
         }
@@ -49,15 +86,15 @@ namespace EvolutionSimulation
                 sizeX = config.heightMap.GetLength(0);
                 sizeY = config.heightMap.GetLength(1);
 
-                if (defHumidity && (sizeX != config.humidityMap.GetLength(0) || sizeY != config.humidityMap.GetLength(1))) { throw new ApplicationException("The provided maps aren't of the same size"); }
-                if (defTemp && (sizeX != config.temperatureMap.GetLength(0) || sizeY != config.temperatureMap.GetLength(1))) { throw new ApplicationException("The provided maps aren't of the same size"); }
+                if (defHumidity && (sizeX != config.humidityMap.GetLength(0) || sizeY != config.humidityMap.GetLength(1))) { throw new NotSameSizeMapsException("The provided maps aren't of the same size"); }
+                if (defTemp && (sizeX != config.temperatureMap.GetLength(0) || sizeY != config.temperatureMap.GetLength(1))) { throw new NotSameSizeMapsException("The provided maps aren't of the same size"); }
             }
             else if (defHumidity)
             {
                 sizeX = config.humidityMap.GetLength(0);
                 sizeY = config.humidityMap.GetLength(1);
 
-                if (defTemp && (sizeX != config.temperatureMap.GetLength(0) || sizeY != config.temperatureMap.GetLength(1))) { throw new ApplicationException("The provided maps aren't of the same size"); }
+                if (defTemp && (sizeX != config.temperatureMap.GetLength(0) || sizeY != config.temperatureMap.GetLength(1))) { throw new NotSameSizeMapsException("The provided maps aren't of the same size"); }
             }
             return;
         }
@@ -74,9 +111,9 @@ namespace EvolutionSimulation
                 }
                 catch
                 {
-                    throw new ApplicationException("The provided height function is not defined for the requested input range");
+                    throw new FunctionNotWhollyDefinedException("The provided height function is not defined for the requested input range");
                 }
-                if (val < 0 || val > 1) throw new ApplicationException("The provided height function has an incorrect output range");
+                if (val < 0 || val > 1) throw new FunctionIncorrectOutputRangeException("The provided height function has an incorrect output range");
             }
             return;
         }
@@ -93,9 +130,9 @@ namespace EvolutionSimulation
                 }
                 catch
                 {
-                    throw new ApplicationException("The provided influence function is not defined for the requested input range");
+                    throw new FunctionNotWhollyDefinedException("The provided influence function is not defined for the requested input range");
                 }
-                if (val < -1 || val > 1) throw new ApplicationException("The provided influence function has an incorrect output range");
+                if (val < -1 || val > 1) throw new FunctionIncorrectOutputRangeException("The provided influence function has an incorrect output range");
             }
             return;
         }
@@ -113,9 +150,9 @@ namespace EvolutionSimulation
                     }
                     catch
                     {
-                        throw new ApplicationException("The provided flora function is not defined for the requested input range");
+                        throw new FunctionNotWhollyDefinedException("The provided flora function is not defined for the requested input range");
                     }
-                    if (val < 0 || val > 1) throw new ApplicationException("The provided flora function has an incorrect output range");
+                    if (val < 0 || val > 1) throw new FunctionIncorrectOutputRangeException("The provided flora function has an incorrect output range");
                 }
             return;
         }
@@ -133,9 +170,9 @@ namespace EvolutionSimulation
                     }
                     catch
                     {
-                        throw new ApplicationException("The provided flora function is not defined for the requested input range");
+                        throw new FunctionNotWhollyDefinedException("The provided flora function is not defined for the requested input range");
                     }
-                    if (val < 0 || val > 1) throw new ApplicationException("The provided flora function has an incorrect output range");
+                    if (val < 0 || val > 1) throw new FunctionIncorrectOutputRangeException("The provided flora function has an incorrect output range");
                 }
             return;
         }
@@ -152,9 +189,9 @@ namespace EvolutionSimulation
                 }
                 catch
                 {
-                    throw new ApplicationException("The provided height function is not defined for the requested input range");
+                    throw new FunctionNotWhollyDefinedException("The provided height function is not defined for the requested input range");
                 }
-                if (val < 0 || val > 4) throw new ApplicationException("The provided flora selector function has an incorrect output range");
+                if (val < 0 || val > 4) throw new FunctionIncorrectOutputRangeException("The provided flora selector function has an incorrect output range");
             }
             return;
         }
