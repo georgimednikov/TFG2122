27f66374192f87bd253d18635916208b648fa21d
Daniel Cortijo <dcortijo@ucm.es>
2022-03-13 17:09:08 +0100

    Parámetro experimental de restExpense
    Ahora tiene un máximo de tiempo hasta caerse de sueño, y el expense se determina cogiendo el expense de este tiempo, y si tiene mucha resistencia quitarle hasta la mitad, y si no tiene nada, dejarlo como estaba.

--- Evolution/EvolutionSimulation/src/Entities/Animal.cs
+++ Evolution/EvolutionSimulation/src/Entities/Animal.cs
@@ -111,9 +111,12 @@ namespace EvolutionSimulation.Entities
 
             stats.MaxRest = resourceAmount;
             stats.CurrRest = stats.MaxRest;
-            stats.RestExpense = minRestExpense + (maxRestExpense - minRestExpense) * // TODO: Numeros arcanos
-                (1 - (float)chromosome.GetFeature(CreatureFeature.Resistance) / chromosome.GetFeatureMax(CreatureFeature.Resistance));
-            stats.RestRecovery = stats.RestExpense * exhaustToSleepRatio;
+            float re = 1f / (UniverseParametersManager.parameters.hoursTilExhaustion * UniverseParametersManager.parameters.ticksPerHour);
+            stats.RestExpense = re / 2f +   // TODO: Numeros arcanos
+                (re / 2f * (1 - (float)chromosome.GetFeature(CreatureFeature.Resistance) / chromosome.GetFeatureMax(CreatureFeature.Resistance)));
+            //    minRestExpense + (maxRestExpense - minRestExpense) * 
+            //    (1 - (float)chromosome.GetFeature(CreatureFeature.Resistance) / chromosome.GetFeatureMax(CreatureFeature.Resistance));
+            //stats.RestRecovery = stats.RestExpense * exhaustToSleepRatio;
 
             //Environment related stats
             stats.Camouflage = chromosome.GetFeature(CreatureFeature.Camouflage);
