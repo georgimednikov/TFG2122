c624883e3fec09479ed8b083fbcf889614678094
Daniel Cortijo <dcortijo@ucm.es>
2022-03-28 20:56:17 +0200

    Arreglado ChaseEnemy
    Ahora se reasigna el camino y objetivo independiente y correctamente.
    O eso esperemos.

--- Evolution/EvolutionSimulation/src/FSM/Creature/States/Combat/ChaseEnemy.cs
+++ Evolution/EvolutionSimulation/src/FSM/Creature/States/Combat/ChaseEnemy.cs
@@ -10,7 +10,7 @@ namespace EvolutionSimulation.FSM.Creature.States
         // Position and ID of the objective
         int objectiveID;
         Vector2Int objective;
-        string speciesName;
+        string objSpecies;
 
         // In case the chase ends and it does not end up attacking
         bool brake = false; // TODO: esto en teoria no deberia pasar nunca ?
@@ -34,11 +34,15 @@ namespace EvolutionSimulation.FSM.Creature.States
         public override void OnEntry()
         {
             creature.Enemy(out objectiveID, out objective);
-            speciesName = creature.world.GetCreature(objectiveID).speciesName;
-            if(objective.x != creature.x || objective.y != creature.y)
+            objSpecies = creature.world.GetCreature(objectiveID).speciesName;
+            if (objective.x != creature.x && objective.y != creature.y)
+            {
                 creature.SetPath(objective);   // This MUST be set up for the cost of the action to work
+            }
             else
+            {
                 brake = true;
+            }
         }
 
         public override void Action()
@@ -48,13 +52,13 @@ namespace EvolutionSimulation.FSM.Creature.States
                 creature.Place((int)nextPos.X, (int)nextPos.Y, (Entities.Creature.HeightLayer)nextPos.Z);
             }
 
-            int otherID; Vector2Int obj;
-            creature.Enemy(out otherID, out obj);   // This is NOT cached because objective can change to another creature
+            int otherID; Vector2Int otherObj;
+            creature.Enemy(out otherID, out otherObj);   // This is NOT cached because objective can change to another creature
 
             if(otherID == -1)   // Target has died! This entails looking for another ONLY if there is a menace, or none if there is not
             {                   // This will only take place when the creature still feels threatened and is cornered, because it won't be able to flee
-                creature.Menace(out otherID, out obj);  
-                if(Math.Abs(obj.x - creature.x) >= creature.stats.Perception / 2 || Math.Abs(obj.y - creature.y) >= creature.stats.Perception / 2)
+                creature.Menace(out otherID, out otherObj);  
+                if(Math.Abs(otherObj.x - creature.x) >= creature.stats.Perception / 2 || Math.Abs(otherObj.y - creature.y) >= creature.stats.Perception / 2)    // TODO: ajustar criterio de no cornered
                 {   // Creature is sufficiently far away to not pose immediate danger
                     creature.cornered = false;
                     brake = false;
@@ -62,19 +66,19 @@ namespace EvolutionSimulation.FSM.Creature.States
                 }
             }
 
-            if (otherID != objectiveID)  // If objective is a different one, adjust accordingly
+            if(otherID != objectiveID)  // If objective creature is a different one, adjust accordingly
             {
                 objectiveID = otherID;
-                speciesName = creature.world.GetCreature(objectiveID).speciesName;
-                objective = obj;
-                creature.TargetEnemy(otherID);  // Redundant, except when current target dies
-                creature.SetPath(objective);    // Set the path the creature must follow
+                objSpecies = creature.world.GetCreature(objectiveID).speciesName;
+                creature.TargetEnemy(otherID);  // Redundant, except when current target is the menace
             }
-            if (objective != obj && obj.x != creature.x && obj.y != creature.y)
+
+            if (otherObj.x != objective.x || otherObj.y != objective.y)  // If objective position is a different one, adjust accordingly
             {
-                objective = obj;
+                objective = otherObj;
                 creature.SetPath(objective);    // Set the path the creature must follow
             }
+
             brake = false;
         }
 
@@ -86,7 +90,7 @@ namespace EvolutionSimulation.FSM.Creature.States
 
         public override string GetInfo()
         {
-            return creature.speciesName + " with ID: " + creature.ID + " CHASES " + speciesName + " with ID: " + objectiveID;
+            return creature.speciesName + " with ID: " + creature.ID + " CHASES " + objSpecies + " with ID: " + objectiveID;
         }
 
         public override string ToString()
