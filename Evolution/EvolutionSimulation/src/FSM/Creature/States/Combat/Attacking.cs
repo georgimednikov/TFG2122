e43b652c029375972aef01f5336fc0a7064fc3dc
Daniel Cortijo <dcortijo@ucm.es>
2022-05-03 10:33:39 +0200

    Evento de envenenamiento

--- Evolution/EvolutionSimulation/src/FSM/Creature/States/Combat/Attacking.cs
+++ Evolution/EvolutionSimulation/src/FSM/Creature/States/Combat/Attacking.cs
@@ -36,11 +36,14 @@ namespace EvolutionSimulation.FSM.Creature.States
                 if (poison)
                 {
                     // TODO: Poison Event
+                    Tracker.Instance.Track(new CreatureApplyPoison(creature.world.tick, creature.ID, creature.speciesName, 
+                        objCreature.ID, objCreature.speciesName, creature.stats.Venom * 0.25f, (int)(creature.stats.Venom)));
                     objCreature.ReceiveInteraction(creature, Entities.Interactions.poison);
                 }
                 objCreature.ReceiveInteraction(creature, Entities.Interactions.attack);
 
-                Tracker.Instance.Track(new CreatureAttack(creature.world.tick, creature.ID, creature.speciesName, objCreature.ID, creature.stats.Damage, creature.stats.Perforation));
+                Tracker.Instance.Track(new CreatureAttack(creature.world.tick, creature.ID, creature.speciesName, 
+                    objCreature.ID, objCreature.speciesName, creature.stats.Damage, creature.stats.Perforation));
             }
         }
 
---
+++ Evolution/Telemetry/Events/CreatureApplyPoison.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Telemetry.Events
+{
+    public class CreatureApplyPoison : CreatureEvent
+    {
+        public int victimID { get; private set; }
+        public string victimSpecies { get; private set; }
+        public float damage { get; private set; }
+        public int duration { get; private set; }
+        public CreatureApplyPoison(int tick, int creatureID, string speciesName, int vID, string vSp, float dmg, int dur) : base(EventType.CreatureAttack, tick, creatureID, speciesName) 
+        { victimID = vID; victimSpecies = vSp; damage = dmg; duration = dur; }
+    }
+}
